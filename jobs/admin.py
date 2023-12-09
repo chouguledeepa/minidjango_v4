5301e0d4670e76d7905be727821727d740d4d31a
chouguledeepa <chouguledeepa99@gmail.com>
2023-12-09 18:08:51 +0530

    jobs url

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 # Register your models here.
 
+from jobs.models import Portal #JobTitle
+
+
+#admin.site.register(JobDescription)
+#admin.site.register(Applicant)
+admin.site.register(Portal)
+#admin.site.register(JobTitle)
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from jobs.models import Portal
+
+
 # Create your views here.
 
 def welcome(request):
     return HttpResponse("<p>welcome to job applications</p>")
+
+
+def portal_details(request):
+    objs = Portal.objects.all()
+    portals = []
+    for obj in objs:
+        portals.append(obj.name)
+    final='=='.join(portals)
+    return HttpResponse(f"<p>{final}</p>")
